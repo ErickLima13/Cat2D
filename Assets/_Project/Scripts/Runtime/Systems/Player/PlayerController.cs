6fe8fb161a9fd358a6394d93d7f55befb7027c19
Erick Lima Araujo <erickequipe7@gmail.com>
2023-05-03 10:03:14 -0300

    Added animation for waiting

--- Assets/_Project/Scripts/Runtime/Systems/Player/PlayerController.cs
+++ Assets/_Project/Scripts/Runtime/Systems/Player/PlayerController.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -8,6 +9,7 @@ public class PlayerController : MonoBehaviour
     private float horizontal;
 
     [SerializeField] private bool isOnTheGround;
+    [SerializeField] private bool isWaiting;
 
     [Range(0, 1000)] public float jumpForce;
     [Range(0, 50)] public float speed;
@@ -17,6 +19,7 @@ public class PlayerController : MonoBehaviour
     public LayerMask groundLayer;
 
     public bool isLeft;
+
 
     private void Initialization()
     {
@@ -40,6 +43,7 @@ public class PlayerController : MonoBehaviour
         MovementControl();
         Jump();
         AnimationsControl();
+        StartWaiting();
     }
 
     private void MovementControl()
@@ -88,6 +92,28 @@ public class PlayerController : MonoBehaviour
         playerAnimator.SetInteger("SpeedX", (int)horizontal);
         playerAnimator.SetBool("Grounded", isOnTheGround);
         playerAnimator.SetFloat("SpeedY", playerRigidBody2D.velocity.y);
+
+       
+    }
+
+    private void StartWaiting()
+    {
+        if (horizontal == 0 && isOnTheGround && !isWaiting)
+        {
+            isWaiting = true;
+            StartCoroutine(TiredOfWaiting());
+        }
+        else if (horizontal != 0 || !isOnTheGround)
+        {
+            isWaiting = false;
+            StopCoroutine("TiredOfWaiting");
+        }
+    }
+
+    private IEnumerator TiredOfWaiting()
+    {
+        yield return new WaitForSeconds(8);
+        playerAnimator.SetTrigger("Idle");
     }
 
 }
